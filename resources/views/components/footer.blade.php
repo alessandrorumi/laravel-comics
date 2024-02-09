2eba26c940aa6fa927fe18a38e44f49a7d792644
alessandrorumi <rumi.alessandro21@gmail.com>
2024-02-09 17:19:03 +0100

    Add: Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,83 @@
 <footer>
-    <h1>Hello from footer</h1>
+  <div class="footer-top">
+    <div class="footer-resources">
+      <div class="dc-shop">
+        <div class="dc-comics-list">
+          <h3>DC COMICS</h3>
+          <ul>
+            <li><a href="">Characters</a></li>
+            <li><a href="">Comics</a></li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">Games</a></li>
+            <li><a href="">Videos</a></li>
+            <li><a href="">News</a></li>
+          </ul>
+        </div>
+        <div class="shop">
+          <h3>SHOPS</h3>
+          <ul>
+            <li><a href="">Shop DC</a></li>
+            <li><a href="">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="dc">
+        <div class="dc-list">
+          <h3>DC</h3>
+          <ul>
+            <li><a href="">Terms Of Use</a></li>
+            <li><a href="">Privacy policy (New)</a></li>
+            <li><a href="">Ad Choices</a></li>
+            <li><a href="">Advertising</a></li>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Subscriptions</a></li>
+            <li><a href="">Talent Workshops</a></li>
+            <li><a href="">CPSC Certificates</a></li>
+            <li><a href="">Ratings</a></li>
+            <li><a href="">Shop Help</a></li>
+            <li><a href="">Contact Us</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="sites">
+        <div class="sites-list">
+          <h3>SITES</h3>
+          <ul>
+            <li><a href="">DC</a></li>
+            <li><a href="">MAD Magazines</a></li>
+            <li><a href="">DC Kids</a></li>
+            <li><a href="">DC Universe</a></li>
+            <li><a href="">DC Power Visa</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-logo-img">
+      <img src="../assets/img/dc-logo-bg.png" alt="">
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="sign-up">
+      <a href="">SIGN-UP NOW!</a>
+    </div>
+    <div class="socials">
+      <a href="">FOLLOW US</a>
+      <div class="facebook">
+        <a href="#"><img src="{{ Vite::asset('/resources/img/footer-facebook.png') }}" alt=""></a>
+      </div>
+      <a href="#"><img src="{{ Vite::asset('/resources/img/footer-twitter.png') }}" alt=""></a>
+      <div class="twitter">
+      </div>
+      <a href="#"><img src="{{ Vite::asset('/resources/img/footer-youtube.png') }}" alt=""></a>
+      <div class="youtube">
+      </div>
+      <a href="#"><img src="{{ Vite::asset('/resources/img/footer-pinterest.png') }}" alt=""></a>
+      <div class="pinterest">
+      </div>
+      <a href="#"><img src="{{ Vite::asset('/resources/img/footer-periscope.png') }}" alt=""></a>
+      <div class="location">
+      </div>
+    </div>
+  </div>
 </footer>
